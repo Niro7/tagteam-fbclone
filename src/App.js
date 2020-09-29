f50c39c80c22a69f29df90b10775a9b03e5aa40d
K.Niroshani <niroshini.07.skk@gmail.com>
2020-09-29 12:03:27 +0530

    Initialize App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Let's Build FACEBOOK!!!! 🔥🚀</h1>
+    </div>
+  );
+}
+
+export default App;
